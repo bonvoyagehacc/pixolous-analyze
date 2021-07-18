2392170ecaa7fc3f38d550cf8b17cb6161b14d56
Nithin <kathrikai30201094@gmail.com>
2021-07-18 06:45:32 +0000

    added haarcascade files

--- eyes.go
+++ eyes.go
@@ -1,1 +1,26 @@
 package pixolousAnalyze
+
+import (
+	"fmt"
+
+	"gocv.io/x/gocv"
+)
+
+func DetectOpenEyes(path string) {
+	face_cascade := gocv.NewCascadeClassifier()
+	defer face_cascade.Close()
+
+	if !face_cascade.Load("haarcascade_frontalface_default.xml") {
+		fmt.Printf("Error reading cascade file: %v\n", "haarcascade_frontalface_default.xml")
+		return
+	}
+
+	eye_cascade := gocv.NewCascadeClassifier()
+	defer eye_cascade.Close()
+
+	if !eye_cascade.Load("haarcascade_eye_tree_eyeglasses.xml") {
+		fmt.Printf("Error reading cascade file: %v\n", "haarcascade_eye_tree_eyeglasses.xml")
+		return
+	}
+
+}
